f5f9701f834573a93b645f251a938ec666ce2b1a
Janelith Rodriguez <janelithrodriguez@hotmail.com>
2024-11-08 15:44:43 -0300

    Agregar archivos de proyecto.

---
+++ RegistroCitas.Server/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+
+//-----------------------------------------------------------------------------------------------------
+//Configuracion de los servicios en el constructor de la aplicacion
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+
+builder.Services.AddDbContext<Context>(op => op.UseSqlServer("name=conn"));
+
+//------------------------------------------------------------------------------------------------------
+//construcción de la aplicación
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
